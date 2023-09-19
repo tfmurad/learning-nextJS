e3d8e93c28c0d88340e1bf1b9a4321151198b48c
Themefisher <murad.themefisher@gmail.com>
2023-09-19 10:44:21 +0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I'm Murad - React Developer As an accomplished web developer, I take
+          great pride in creating sophisticated, responsive, and highly
+          functional websites that seamlessly integrate cutting-edge
+          technologies with imaginative design solutions.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
